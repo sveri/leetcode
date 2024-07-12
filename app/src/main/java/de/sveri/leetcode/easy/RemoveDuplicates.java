041a20a04b828d031cc70049279313461660d563
Sven Richter <sveri80@gmail.com>
2024-07-12 17:31:50 +0200

    remove duplicates

--- app/src/main/java/de/sveri/leetcode/easy/RemoveDuplicates.java
+++ app/src/main/java/de/sveri/leetcode/easy/RemoveDuplicates.java
@@ -1,5 +1,41 @@
 package de.sveri.leetcode.easy;
 
+import java.util.HashSet;
+
 public class RemoveDuplicates {
+
+	public int removeDuplicates(int[] nums) {
+
+		if (nums.length <= 1) {
+			return nums.length;
+		}
+
+		int uniques = 0;
+
+		var s = new HashSet<Integer>();
+
+		int lastInt = Integer.MIN_VALUE;
+		int lastUniqueIndex = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+
+			int numAtI = nums[i];
+
+			if (nums[i] > lastInt) {
+				nums[lastUniqueIndex] = nums[i];
+			}
+
+			if (!s.contains(numAtI)) {
+				s.add(numAtI);
+				lastUniqueIndex++;
+				uniques++;
+				lastInt = nums[i];
+			}
+
+		}
+
+		return uniques;
+
+	}
 
 }
---
+++ app/src/test/java/de/sveri/leetcode/RemoveDuplicatesTest.java
@@ -0,0 +1,30 @@
+package de.sveri.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import de.sveri.leetcode.easy.RemoveDuplicates;
+
+public class RemoveDuplicatesTest {
+
+	@Test
+	void testSimple() throws Exception {
+		testIt(new int[] { 1, 2 }, 2, new int[] { 1, 2 });
+		testIt(new int[] {}, 0, new int[] { 1, 1 });
+		testIt(new int[] { 1 }, 1, new int[] { 1, 1 });
+		testIt(new int[] { 1, 1 }, 1, new int[] { 1, 1 });
+		testIt(new int[] { 1, 1, 2 }, 2, new int[] { 1, 2 });
+		testIt(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5, new int[] { 0, 1, 2, 3, 4, 8, 9, 10 });
+	}
+
+	private void testIt(int[] numsIn, int expectedUniqeus, int[] expectedNums) {
+		int countUniques = new RemoveDuplicates().removeDuplicates(numsIn);
+		assertEquals(expectedUniqeus, countUniques);
+//		var expected = new int[] { 1, 2 };
+		for (int i = 0; i < countUniques; i++) {
+			assertEquals(expectedNums[i], numsIn[i]);
+		}
+	}
+
+}
